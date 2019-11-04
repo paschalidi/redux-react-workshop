fe6c5548212f30e79120cfb975a1a377286108bd
Christos Paschalidis <paschalidi@outlook.com>
2019-11-04 18:40:55 +0100

    text

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { connect } from "react-redux";
 function App({ countReducer, dispatch }) {
   return (
     <div>
-      <button onClick={() => dispatch({ type: "COUNT_UP" })}>add by 1</button>
+      <button onClick={() => dispatch({ type: "COUNT_UP" })}>
+        increase by 1
+      </button>
       <button onClick={() => dispatch({ type: "COUNT_DOWN" })}>
-        subtract by 1
+        decrease by 1
       </button>
       <div>{countReducer}</div>
     </div>
